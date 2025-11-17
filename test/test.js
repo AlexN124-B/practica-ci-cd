6d96a33266a7914faecde4b10d1fa5f4ea0ade91
Alejandro Casa <an658716@gmail.com>
2025-11-17 17:35:45 +0100

    Pruebas workflows

--- test/test.js
+++ test/test.js
@@ -29,6 +29,6 @@ describe('GET', function(){
 		      request
 		      .get('/api')
 		      .set('Accept', 'application/json')
-		      .expect(200, {"cat":"meow","dog":"bark","eel":"hiss","bear":"growl","frog":"croak","lion":"roar","bird":"tweet", "elephant" : "uuuuuuu", "texto2": "texto2"}, done);
+		      .expect(200, {"cat":"meow","dog":"bark","eel":"hiss","bear":"growl","frog":"croak","lion":"roar","bird":"tweet", "elephant" : "uuuuuuu"}, done);
 		    })
 })
